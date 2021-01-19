17213b3104c63d8236255b5e463b4b518a76d8b5
billygr <billygr@users.noreply.github.com>
2021-01-19 16:35:39 +0200

    Version 0.6 bugfixes, buttons, 2 set of MOSFETS

--- electronicload/electronicload.ino
+++ electronicload/electronicload.ino
@@ -10,15 +10,9 @@
  * SW version 0.5 to suit PCB version 0.9.
  
  Release notes:
- *** SW version 0.5: First published version. 
- * NOT YET DONE/VERIFIED!!:
- - Change the original SW to use an I2C LCD (started, but not verified)
- - Change pin assignments to suit new PCB connections (started, but not verified)
- - Change the code to only use two sets of MOS-FETs to ensure load calc is correct (ln. 165 and forward)
+ *** SW version 0.6: working with PCB version 0.9
  - Optional: Set up and use AUX-pins on the board to measure fan speed, heat sink temeperature etc.
- 
- *** PCB version 0.9: First published (untested) version.
- 
+ - FIXME: Ensure that the voltage is measured correctly
 */
 
 #include <SPI.h>
@@ -48,7 +42,7 @@ const int IDX_BTN_MODE = 2;
 const int IDX_BTN_ENC = 3;
 const int LOOP_MAX_COUNT = 2000;
 
-int DACGain = 1; //default gain 1xVref. 
+int DACGain = 1; //default gain 1xVref.
 
 int loadMode = 0; // 0: Constant Current, 1: Constant Power
 const float EXT_REF_VOLTAGE = 0.333; // MPC4921 Reference voltage used, adjust it using the trimmer
@@ -65,10 +59,10 @@ LiquidCrystal_PCF8574 lcd(I2Cadr);  //set up the LCD address
 Encoder currentAdjEnc(2, 3); //encoder connected to pins 2 & 3
 
 int buttons[]={BTN_RESET, BTN_RANGE_X2, BTN_MODE, BTN_ENC};
-int buttonReadings[3];
+int buttonReadings[4];
 int lastButtonStates[]={HIGH, HIGH, HIGH, HIGH};
 int currentButtonStates[]={HIGH, HIGH, HIGH, HIGH};
-long lastDebounceTime[3];
+long lastDebounceTime[4];
 
 long oldEncPosition = -999;
 long curEncPosition = 0;
@@ -84,7 +78,7 @@ float setPower = 0.0;
 float setCurrent = 0.0;
 
 
-//***START OF MAIN PROGRAM***START OF MAIN PROGRAM***START OF MAIN PROGRAM***START OF MAIN PROGRAM***START OF MAIN PROGRAM 
+
 
 void setup()
 {
@@ -92,9 +86,9 @@ void setup()
         pinMode(buttons[i], INPUT);
         digitalWrite(buttons[i], HIGH);
     }
-    
+
     pinMode(PIN_CS, OUTPUT);
-    SPI.begin();  
+    SPI.begin();
     SPI.setClockDivider(SPI_CLOCK_DIV2);
 
     lcd.begin(LCDcols, LCDrows);
@@ -115,9 +109,9 @@ void getCurrentEncPosition()
             
             if (encoderValue < 0) encoderValue = 0;
         }
-        
+
         oldEncPosition = curEncPosition;
-        
+
         if (loadMode == 0) //Constant Current mode
         {
             setDACOutput(encoderValue);
@@ -137,12 +131,12 @@ void setDACOutput(unsigned int val)
     PORTB &= 0xfb;
     SPI.transfer(hByte);
     SPI.transfer(lByte);
-    PORTB |= 0x4;    
+    PORTB |= 0x4;
 }
 
 void displayStatus()
-{   
-    lcd.clear();     
+
+    lcd.clear();
 
     //average load voltage
     vLoad = ADSum * 1.0 / (float) LOOP_MAX_COUNT / 1024.0 * 5.0  * (RA+RB) / RB; //(RA+RB)/RB
@@ -154,7 +148,7 @@ void displayStatus()
         if  (DACGain == 0) i *=2; // x2
     
         lcd.print("CI,I Set=");
-    
+
         if (i < 1.0) {
             lcd.print(i * 1000,0);
             lcd.print(" mA");
@@ -165,61 +159,61 @@ void displayStatus()
     } else { //Constant Power
         setPower = (float) encoderValue / 20.0; //approximately 0-200W
 
-        lcd.print("CP, P Set=");        
+        lcd.print("CP, P Set=");
         if (vLoad > 0.5) {//minimum 0.5V
             setCurrent = setPower / (float) vLoad;
-            
+
             //desired sense voltage. Since we have 3 ->> CHANGE TO USE TWO SETS OF MOSFETS!!
             //sets of MOSFETS, the results are devided
             //by 3 and multipled by the value of the sense
             //resistor
             float vSense = setCurrent / 2.0 * 0.1; //FIXED: Change it to 2 sets of MOSFET
-          
+
             DACSetValue = (int) (vSense/EXT_REF_VOLTAGE * 4096.0 + 0.5);
 
             if (setCurrent < 10.0) {
-                DACGain = 1;                
+                DACGain = 1;
             } else {
                 DACGain = 0;
                 DACSetValue = (int) ((float) DACSetValue / 2.0 + 0.5);
-            }                        
-            
+            }
+
             if (setPower >= 100.0) {
                 lcd.print(setPower, 1);
             }
             else {
-               lcd.print(setPower, 2);     
+               lcd.print(setPower, 2);
             }
         } else {
             setPower = 0;
             lcd.print(setPower, 2);
         }
-        
+
         setDACOutput(DACSetValue);
         lcd.print("W");
-    } 
-    
+    }
+
     lcd.setCursor(0, 1);
     lcd.print("LOAD V=");
     lcd.print(vLoad, 2);
-    lcd.print(" V");        
+    lcd.print(" V");
 }
 
-//***START OF LOOP SECTION***START OF LOOP SECTION***START OF LOOP SECTION***START OF LOOP SECTION***START OF LOOP SECTION
+
 
 void loop()
 {
-    int idx = 0;
-    
+
+
     for (int i = 0 ; i < 4; i++) {
         buttonReadings[i] = digitalRead(buttons[i]);
-        
+
         if (buttonReadings[i] != lastButtonStates[i]) lastDebounceTime[i] = millis();
-        
+
         if (millis() - lastDebounceTime[i] > 50) { //debouncing the buttons
             if (currentButtonStates[i] != buttonReadings[i]) {
-                currentButtonStates[i] = buttonReadings[i];                                
-                
+                currentButtonStates[i] = buttonReadings[i];
+
                 //actions
                 if (currentButtonStates[i] == LOW) {
                     switch (i) {
@@ -233,7 +227,7 @@ void loop()
                         case IDX_BTN_RANGE_X2:
                         //switch between 100W/200W maximum power mode
                             DACGain = DACGain == 1? 0 : 1;
-                            
+
                             if (loadMode == 0) {
                                 setDACOutput(encoderValue);
                             }
@@ -245,7 +239,7 @@ void loop()
                         case IDX_BTN_ENC:
                         //cycle through different encoder steps: 1/10/100
                             DACSetStep *= 10;
-                            
+
                             if (DACSetStep > 100) DACSetStep = 1;
                             break;
                     }
@@ -254,13 +248,13 @@ void loop()
         }
         
         lastButtonStates[i] = buttonReadings[i];
-    }        
+    }
 
-    getCurrentEncPosition();    
+    getCurrentEncPosition();
     loopCounter++;
-    
+
     // used to smooth out the analogRead results
-    ADSum += analogRead(pinLoadVoltage); 
+    ADSum += analogRead(pinLoadVoltage);
     
     if (loopCounter == LOOP_MAX_COUNT) {
         displayStatus();
